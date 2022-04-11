5f24bc55348a052dd0145dc1217c43fa3dd5a8a9
Aleksandr Fedotov <qwfwr2@yandex.ru>
2022-04-11 23:35:47 +0300

    auth

--- cats/views.py
+++ cats/views.py
@@ -1,9 +1,49 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from .models import Cat
-from .serializers import CatSerializer
+from .models import Cat, Owner, Achievement
+from .serializers import (
+    CatSerializer, OwnerSerializer, AchievementSerializer,
+    CatListSerializer
+)
+
+
+class CreateRetrieveViewSet(
+    mixins.CreateModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet
+):
+    pass
+
+
+class LightCatViewSet(CreateRetrieveViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+
+    # Возвращает из Get-запроса 5 последних белых котов
+    @action(detail=False, url_path='recent-white-cats')
+    def recent_white_cat(self, request):
+        cats = Cat.objects.filter(color='White')[:5]
+        serializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data)
+
+    # выбор сериализатора для вьюсета
+    def get_serializer_class(self):
+        # Если запрошенное действие (action) — получение списка объектов
+        if self.action == 'list':
+            return CatListSerializer
+        return CatSerializer
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+
+
+class AchievementViewSet(viewsets.ModelViewSet):
+    queryset = Achievement.objects.all()
+    serializer_class = AchievementSerializer
